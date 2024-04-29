43c38ae8dc1e9372a78f98f48db672516c73c24e
AWKRID <minzoo202738@gmail.com>
2024-04-29 14:40:41 +0900

    :sparkles: feat: 유저 입력 Validation 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,30 @@
 package org.example
 
 var answer = "456"
+const val level = 3
 fun main() {
 
 
     var userTry: String
     println("<게임을 시작합니다>")
-    makeAnswer(3)
+    makeAnswer(level)
     println(answer)
     while (true) {
         userTry = input("숫자를 입력하세요\n")
+        if(!isValidInput(userTry)){
+            println("올바르지 않은 입력값입니다.")
+            continue
+        }
         if (checkAnswer(userTry)) break
     }
 
+}
+
+fun isValidInput(userInput: String): Boolean {
+    if (userInput.length != level) return false
+    if (userInput.toCharArray().distinct().size != level) return false
+    if(userInput.contains("0")) return false
+    return true
 }
 
 fun checkAnswer(userTry: String): Boolean {
@@ -44,8 +56,8 @@ fun input(message: String): String {
 }
 
 fun makeAnswer(size: Int) {
-    val numberArray = IntArray(9) { it+1 }
-    repeat(size){
+    val numberArray = IntArray(9) { it + 1 }
+    repeat(size) {
         val firstNumber = numberArray.indices.random()
         val secondNumber = numberArray.indices.random()
         val temp = numberArray[firstNumber]
@@ -53,7 +65,7 @@ fun makeAnswer(size: Int) {
         numberArray[secondNumber] = temp
     }
     answer = ""
-    for(i in 0..<size){
+    for (i in 0..<size) {
         answer += numberArray[i].toString()
     }
 }
